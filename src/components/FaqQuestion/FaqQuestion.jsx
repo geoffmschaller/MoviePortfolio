6133559ad1d34ea5d1066aa886208b83e1256cad
geoffmschaller <32310187+geoffmschaller@users.noreply.github.com>
2020-11-13 10:38:01 -0800

    FAQ Animation

--- src/components/FaqQuestion/FaqQuestion.jsx
+++ src/components/FaqQuestion/FaqQuestion.jsx
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './FaqQuestion.module.sass';
 
 const FaqQuestion = props => {
+
+	const [isOpen, setIsOpen] = useState(false);
+
+	const toggleQuestion = () => {
+		setIsOpen(!isOpen);
+	}
+
 	return (
-		<div className={styles.faqQuestion}>
+		<div className={styles.faqQuestion} onClick={toggleQuestion}>
 			<div className={styles.question}>
 				<div>{props.question}</div>
-				<i className="far fa-caret-down"/>
+				<i className={`far fa-caret-${isOpen ? 'up' : 'down'}`}/>
 			</div>
 			<div className={styles.border}/>
-			<div className={styles.answer}>
+			<div className={[styles.answer, isOpen ? styles.open : ''].join(" ")}>
 				<div className={styles.header}>Lorem ipsum dolor sit amet.</div>
 				<div className={styles.text}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis nesciunt unde repudiandae natus dicta eligendi ab ipsam sequi fuga architecto aliquid sed perferendis quam, aperiam deleniti, consequatur beatae quasi minima.</div>
 			</div>
